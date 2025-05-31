78adf06cbb38233efc8d8972127f256480bb5755
brightonchebor <cheborbrighton805@gmail.com>
2025-05-31 20:05:17 +0300

    cn

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 app_name = 'app'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home, name='home'),
+    path('check/', check, name='check'),
+    path('login/', logout_view, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout_view, name='logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def home(request):
 
-    return render(request, 'app/login.html')
+    return render(request, 'app/home.html')
+
+def check(request):
+
+    return render(render, 'app/check.html')
+
+def login_view(request):
+
+    return render(request, 'app/login.hmtl')
+
+def register(request):
+
+    return render(request, 'app/register.hmtl')
+
+def logout_view(request):
+
+    return render(request, 'app/logout.hmtl')
