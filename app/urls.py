2c7efb9bce40bed111fc98562694d98f934c929b
brightonchebor <cheborbrighton805@gmail.com>
2025-05-31 20:19:15 +0300

    cn

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,5 @@ app_name = 'app'
 urlpatterns = [
     path('', home, name='home'),
     path('check/', check, name='check'),
-    path('login/', logout_view, name='login'),
-    path('register/', register, name='register'),
-    path('logout/', logout_view, name='logout'),
+    
 ]
---
+++ auth/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+app_name = 'auth'
+
+urlpatterns = [
+    path('login/', logout_view, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout_view, name='logout'),
+]
