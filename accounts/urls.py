d82e672dac6d9d6a58092e59447a206734b28f0e
brightonchebor <cheborbrighton805@gmail.com>
2025-06-24 16:16:37 +0300

    cn

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
+
+
 
 urlpatterns = [
-    path(),
+    path('accounts/login/', login_view, name='login_user'),
+    path('accounts/register/', register, name='register_user'),
+    path('accounts/logout/', logout_view, name='logout_user'),
 ]
