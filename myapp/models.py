5f3ddf351513340a4b886fcb30dfd38230d34103
brightonchebor <cheborbrighton805@gmail.com>
2025-06-27 14:06:18 +0300

    cn

--- myapp/models.py
+++ myapp/models.py
@@ -3,17 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings 
 
-"""
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('attachee', 'Attachee'),
-        ('supervisor', 'Supervisor'),
-    ]
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='attachee')
-"""
-
 class CustomUser(AbstractUser):
     USER_TYPE_CHOICES = [
         ("staff", "Staff"),
