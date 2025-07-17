a173f75a3f4c6903d894319722c17cd762f04973
brightonchebor <cheborbrighton805@gmail.com>
2025-07-17 15:17:51 +0300

    cn

--- core/settings.py
+++ core/settings.py
@@ -70,15 +70,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
 
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('PG_NAME'),
-        'USER': 'postgres',
-        'PASSWORD': env('PG_PWD'),
-        'HOST': 'gondola.proxy.rlwy.net',
-        'PORT': '46664'
     }
 }
 
