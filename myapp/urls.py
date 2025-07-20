eb9fdb90e5d28d49315a72a63b0d1b42f473e7b3
brightonchebor <cheborbrighton805@gmail.com>
2025-07-20 13:23:59 +0300

    cn

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,12 +8,17 @@ urlpatterns = [
    path('', home, name='home'),
 
    path('users/register/', register, name='register'),
+   path('users/verify-email/', verify_email, name='otp'),
    path('users/login/', login_view, name='login'),
    path('users/logout/', logout_view, name='logout'),
 
    path('users/attendance/', attendance, name='attendance'),
 
    path('dashboard/staff/', StaffDashboardView.as_view(), name='staff_dashboard'),
-   path('dashboard/member/', MemberDashboardView.as_view(), name='member_dashboard')
+   path('dashboard/member/', MemberDashboardView.as_view(), name='member_dashboard'),
+   path('member-attendance/<int:member_id>/', MemberAttendanceDetailView.as_view(), name='member_attendance_detail'),
+   path('export-member-attendance/<int:member_id>/', export_member_attendance_csv, name='export_member_attendance'),
+   path('export-all-attendance/', export_all_attendance_csv, name='export_all_attendance'),
+
 ]
 
