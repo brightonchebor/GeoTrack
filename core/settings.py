68cbecf363ebd485e4aaa1a7f2768ef41387e692
brightonchebor <cheborbrighton805@gmail.com>
2025-07-21 14:46:14 +0300

    cn

--- core/settings.py
+++ core/settings.py
@@ -17,8 +17,8 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['geotrack-production.up.railway.app', 'https://geotrack-production.up.railway.app', 'localhost', '127.0.0.0']
-CSRF_TRUSTED_ORIGINS = ['https://geotrack-production.up.railway.app']
+ALLOWED_HOSTS = ['geotrack-tw6e.onrender.com', 'https://geotrack-tw6e.onrender.com', 'localhost', '127.0.0.0']
+CSRF_TRUSTED_ORIGINS = ['https://geotrack-tw6e.onrender.com']
 
 
 INSTALLED_APPS = [
