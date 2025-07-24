484641b9eaa4596bfdcf5cef0cb5439919381d5d
brightonchebor <cheborbrighton805@gmail.com>
2025-07-24 20:26:40 +0300

    cn

--- core/settings.py
+++ core/settings.py
@@ -193,30 +193,3 @@ JAZZMIN_UI_TWEAKS = {
     }
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
-}
